1ec39e2b99afd49ea8969710f39e7206fd4198d4
Jon Erling Hustadnes <jonerling.hustadnes@proton.me>
2025-03-22 12:28:43 +0100

    feat(helpers): added writeBinaryFile

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,4 @@
-import { isAbsolute } from "path";
+import { isAbsolute, dirname } from "path";
 import * as nodeFs from "fs";
 import { App } from "obsidian";
 
@@ -14,5 +14,26 @@ export async function writeFile(path: string, content: string, app: App) {
 		} catch (error) {
 			console.log(`Error writing ${path}`, error);
 		}
+	}
+}
+
+export async function writeBinaryFile(
+	path: string,
+	content: Uint16Array,
+	overwrite = false,
+) {
+	const filePath = isAbsolute(path)
+		? path
+		: `${this.app.vault.adapter.basePath}/${path}`;
+
+	if (nodeFs.existsSync(filePath) && !overwrite) return;
+
+	const directory = dirname(filePath);
+	if (!nodeFs.existsSync(directory)) nodeFs.mkdirSync(directory);
+
+	try {
+		nodeFs.writeFileSync(filePath, content, { encoding: "binary" });
+	} catch (error) {
+		console.log(`Error writing ${filePath}`, error);
 	}
 }
